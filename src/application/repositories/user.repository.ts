ab10cd959790323dba21f63c727751d1809dbc02
Lautaro Ezequiel Murua <lautaroemurua@gmail.com>
2024-10-31 00:35:40 -0300

    fixes

--- src/application/repositories/user.repository.ts
+++ src/application/repositories/user.repository.ts
@@ -8,15 +8,17 @@ import { UserEntity } from '../../domain/entities/user.entity';
 export class UserRepository {
     constructor(
         @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>
+        private readonly repository: Repository<UserEntity>, // Nombre más claro
     ) {}
 
+    // Buscar usuario por número de teléfono
     async findByPhone(phone: string): Promise<UserEntity | undefined> {
-        return this.userRepository.findOne({ where: { phone } });
+        return this.repository.findOne({ where: { phone } });
     }
 
+    // Crear un nuevo usuario
     async createUser(phone: string): Promise<UserEntity> {
-        const user = this.userRepository.create({ phone });
-        return this.userRepository.save(user);
+        const user = this.repository.create({ phone });
+        return this.repository.save(user);
     }
 }
